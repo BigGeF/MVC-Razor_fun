995860bc90306a252e76e4acc3765a34cf618a22
BigGeF <52731933+BigGeF@users.noreply.github.com>
2020-10-24 08:45:24 -0700

    Octtwentyfour

--- Controllers/HomeControllers.cs
+++ Controllers/HomeControllers.cs
@@ -4,9 +4,27 @@ namespace Razor_Fun.Controllers
     public class HomeController : Controller
     {
         [HttpGet("")]
-        public ViewResult Index()
+        public IActionResult Index()
         {
             return View("razor");
+        }
+        [HttpGet("HiThere")]
+        public IActionResult Hello()
+        {
+            return View("hello");
+        }
+
+        [HttpGet("hello")]
+        public IActionResult Any()
+        {
+            return RedirectToAction("Hello");
+        }
+
+        [HttpGet("hello/{username}/{location}")]
+        public JsonResult Jsontest(string username, string location)
+        {
+            var response = new {user = username, place = location};
+            return Json(response);
         }
     }
 }
